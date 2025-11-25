6928a20c1313b976ca9455c48efe22a0d0a3dc39
Cameron Labelle <labelle.cam3@gmail.com>
2025-11-24 20:33:04 -0500

    Code for assignment 3

---
+++ src/Note.java
@@ -0,0 +1,10 @@
+public class Note {
+    public final static String DO = "Sounds/do.wav";
+    public final static String FA = "Sounds/fa.wav";
+    public final static String LA = "Sounds/la.wav";
+    public final static String MI = "Sounds/mi.wav";
+    public final static String RE = "Sounds/re.wav";
+    public final static String SI = "Sounds/si.wav";
+    public final static String SOL = "Sounds/sol.wav";
+    public final static String DO_OCT = "Sounds/do-octave.wav";
+}
